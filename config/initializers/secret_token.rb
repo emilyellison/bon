dbf66c4617044db25ef8016315f347709737a983
Emily Ellison <emilyellison986@gmail.com>
2012-04-10 15:43:13 -0500

    Initial Commit: Fake Plastic Trees

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bon::Application.config.secret_token = '3989389c097608847902d1c25a1bc748aa747db0361de29f1790517dcae0ce206c83ddac35927e4a25b7f96127bc2fdb0a3bd337ed9318a5535ae902a1cfb48e'
