442a28a8ab9b9f4a7f0685226df08396e58860ca
Emily Ellison <emilyellison986@gmail.com>
2012-04-14 11:05:55 -0500

    Improve Sign Up Testing

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -28,6 +28,13 @@ describe 'user pages' do
       it 'should not create a user' do
         expect { click_button submit }.not_to change(User, :count)
       end
+      
+      describe 'error messages' do
+        before { click_button submit }
+        
+        it { should have_selector('title', text: 'Sign Up') }
+        it { should have_content('error') }
+      end
     end
     
     describe 'with valid information' do
@@ -37,8 +44,17 @@ describe 'user pages' do
         fill_in 'Password',         with: 'foobar'
         fill_in 'Confirm Password', with: 'foobar'
       end
+      
       it 'should create a new user' do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+      
+      describe 'after saving the user' do
+        before { click_button submit }
+        let(:user) {User.find_by_email('example@test.com') }
+        
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Congrats') }
       end
     end
   end
